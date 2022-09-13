3a74a42f5c99364e61c202b11133310504fb9544
lateofrederick <lateofrederick@gmail.com>
2022-09-13 16:37:30 +0000

    configure database

---
+++ config/db.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+const userName = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
+
+function init() {
+  const sequelize = new Sequelize(database, userName, password, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    operatorsAliases: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 20000,
+      idle: 5000,
+    },
+  });
+
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.incidents = require("../models/incident")(sequelize);
+
+  return db;
+}
+
+module.exports = {
+  init,
+};
